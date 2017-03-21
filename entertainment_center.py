ecdf8a041d465231b1b7573a7032713f01f017dd
Tan Wang <timmy940408@me.com>
2017-03-21 18:08:24 -0400

    init

---
+++ entertainment_center.py
@@ -0,0 +1,17 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=-9c3BgWV8io")
+movies = [toy_story,avatar]
+fresh_tomatoes.open_movies_page(movies)
+
+
