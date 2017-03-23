ea277e484d82c08f5497f91b13d971561ff284ce
Tan Wang <timmy940408@me.com>
2017-03-23 15:30:43 -0400

    Fixed PEP8 issues

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,37 @@
-'''  The python file to fetch movie data from TheMovieDB API and display them in a 
-scaffolded webpage    '''
+'''  The python file to fetch movie data from TheMovieDB API and display them 
+in a scaffolded webpage    '''
 import requests
 import json
 import media
 import fresh_tomatoes
 
 
-
-# convert the poster path from API to the full path
 def get_full_path(path_from_api):
+    ''' Convert the poster path from API to the full path '''
     return "http://image.tmdb.org/t/p/w185/"+path_from_api
 
-# get the movie trailer url from API
+
 def get_trailer(movie_id):
-    url = "https://api.themoviedb.org/3/movie/"+str(movie_id)+"/videos?api_key="+API_KEY+"&language=en-US"
+    ''' Get the movie trailer url from API '''
+    url = "https://api.themoviedb.org/3/movie/"+str(movie_id) \
+    +"/videos?api_key="+API_KEY+"&language=en-US"
     payload = "{}"
     response = requests.request("GET", url, data=payload)
-    # since there are many video links coming out, I will just pick the first one
+    # since there are many video links coming out, I will just pick the first
     result_dic = json.loads(response.text)['results'][0]
     youtube_key = result_dic['key']
-    return "https://www.youtube.com/watch?v="+youtube_key
-   
+    return "https://www.youtube.com/watch?v="+youtube_key  
 # Enter your API key here
-API_KEY = "Enter your API key here"
+API_KEY = "878337c301e790447564e6a9915721e5"
 
 # the connection code for using the API
-url = "https://api.themoviedb.org/3/discover/movie?api_key="+API_KEY+"&include_video=true"
+url = "https://api.themoviedb.org/3/discover/movie?api_key="+ \
+API_KEY+"&include_video=true"
 payload = "{}"
 response = requests.request("GET", url, data=payload)
 # the webpage does not look nice after 9 items
 result_dic = json.loads(response.text)['results'][0:9]
-movies=[]
+movies = []
 
 for item in result_dic:
     # fetch the nessesary data
@@ -38,23 +39,17 @@ for item in result_dic:
     image_path = item['poster_path']
     overview = item['overview']
     movie_id = item['id']
-    vote = item['vote_average'] # will use this to change the color of the title in the webpage
+    # will use this to change the color of the title in the webpage
+    vote = item['vote_average'] 
     image_path = get_full_path(image_path)
     trailer_url = get_trailer(movie_id)
 
     # create the instance of movie class
-    new_movie = media.Movie(movie_id,title,overview,image_path,trailer_url,vote)
+    new_movie = media.Movie(movie_id, title, overview, 
+                            image_path, trailer_url, vote)
     movies.append(new_movie)
-        
-    
-
-
-
-
-
-
-
 
 fresh_tomatoes.open_movies_page(movies)
+
 
 
--- media.py
+++ media.py
@@ -4,10 +4,15 @@ import webbrowser
 The Movie class
 Stores all the data for a movie
 '''
+
+
 class Movie():
+    
     ''' This class provides a way to store movie related information '''
     VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
-    def __init__(self, mid, movie_title, movie_storyline, poster_image, trailer_youtube,vote):
+    
+    def __init__(self, mid, movie_title, movie_storyline, 
+                 poster_image, trailer_youtube, vote):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -15,10 +20,10 @@ class Movie():
         self.mid = mid
         self.vote = vote
 
-   
     def show_trailer(self):
         ''' Open a webbrowser to show the trailer of the movie '''
         webbrowser.open(self.trailer_youtube_url)
 
 
     
+
